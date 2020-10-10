edba2883c4c475b1749b30e9df02d4cbca0ee021
Michael Nnaji <mnnaji3@uwo.ca>
2020-10-10 17:21:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ end
 
 def  self.create_table
   sql = <<-SQL
-        CREATE TABLE  students (
+        CREATE TABLE IF NOT EXISTS students (
           id  INTEGER PRIMARY KEY,
           name TEXT,
           grade INTEGER
